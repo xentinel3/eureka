e67e0cda4cd322f7d4cd38a8a7b81229d7f55933
Julio César <ing.juliocesar26@outlook.com>
2022-08-09 00:01:17 -0500

    Spring cloud proyect example with load balancing, eureka working, and spring cloud gateway

---
+++ src/test/java/mx/com/think/sceureka/ScEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.think.sceureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
